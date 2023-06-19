4346e0796be063dc3eee95985a24373c786bdcd4
Joao Galeao <jbgaleao-win@outlook.com>
2023-06-19 19:34:16 -0300

    Ajustes na modal em Grupo de Produtos

--- ControleEstoque.Web/App_Start/BundleConfig.cs
+++ ControleEstoque.Web/App_Start/BundleConfig.cs
@@ -14,12 +14,14 @@ namespace ControleEstoque.Web
                         "~/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js",
+                      "~/Scripts/bootbox.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
