f381752ebdd7d2d9acae62de11b0d611cae69c77
Joao Galeao <jbgaleao-win@outlook.com>
2023-06-19 17:05:02 -0300

    pagina Grupo de Produtos - tela de listagem

--- ControleEstoque.Web/Controllers/CadastroController.cs
+++ ControleEstoque.Web/Controllers/CadastroController.cs
@@ -1,13 +1,23 @@
-﻿using System.Web.Mvc;
+﻿using ControleEstoque.Web.Models;
+using System.Collections.Generic;
+using System.Web.Mvc;
 
 namespace ControleEstoque.Web.Controllers
 {
     public class CadastroController : Controller
     {
+
+        private static List<GrupoProdutoModel> _listaGrupoProduto = new List<GrupoProdutoModel>()
+        {
+            new GrupoProdutoModel() { Id=1, Nome="Livros", Ativo=true },
+            new GrupoProdutoModel() { Id=2, Nome="Mouses", Ativo=true },
+            new GrupoProdutoModel() { Id=3, Nome="Monitores", Ativo=false }
+        };
+
         [Authorize]
         public ActionResult GrupoProduto()
         {
-            return View();
+            return View(_listaGrupoProduto);
         }
 
         [Authorize]
---
+++ ControleEstoque.Web/Models/GrupoProdutoModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ControleEstoque.Web.Models
+{
+    public class GrupoProdutoModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
