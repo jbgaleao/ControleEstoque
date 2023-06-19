349fe6b1c6283fdcf77a15fd6b271173f005bbe5
Joao Galeao <jbgaleao-win@outlook.com>
2023-06-19 00:17:26 -0300

    estrutura MVC

--- Controle-Estoque.Web/Controllers/HomeController.cs
+++ Controle-Estoque.Web/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace Controle_Estoque.Web.Controllers
 {
@@ -13,18 +9,10 @@ namespace Controle_Estoque.Web.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Sobre()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
